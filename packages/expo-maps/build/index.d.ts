ad5f86a3ddb9fca4c6cad5b31ca59d5131d907cc
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2025-02-12 12:21:47 +0100

    [maps][iOS] Implement setCameraPosition

--- packages/expo-maps/build/index.d.ts
+++ packages/expo-maps/build/index.d.ts
@@ -1,5 +1,4 @@
 import * as AppleTypes from './apple/AppleMaps.types';
-import { AppleMapsView } from './apple/AppleMapsView';
 import * as GoogleTypes from './google/GoogleMaps.types';
 import { GoogleStreetView } from './google/GoogleStreetView';
 /**
@@ -23,7 +22,7 @@ export declare namespace GoogleMaps {
  * @hidden
  */
 export declare namespace AppleMaps {
-    const View: typeof AppleMapsView;
+    const View: import("react").ForwardRefExoticComponent<Omit<AppleTypes.AppleMapsViewProps, "ref"> & import("react").RefAttributes<AppleTypes.AppleMapsViewType>>;
     const MapType: typeof AppleTypes.AppleMapsMapType;
     type MapType = AppleTypes.AppleMapsMapType;
     type Marker = AppleTypes.AppleMapsMarker;
